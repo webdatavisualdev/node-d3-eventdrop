c56a675282622330cbdc456340a1ff1b2f4be4bf
webdatavisualdev <cli698638@gmail.com>
2017-12-13 09:01:22 -0800

    Made tooltip

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// Get dependencies
 const express = require('express');
 const path = require('path');
 const http = require('http');
@@ -11,20 +10,12 @@ const API = 'https://app.close.io/api/v1/';
 const token = '8c94e40011d3a9d79cef879e07863727061d830f9a26edaba03dcb62';
 const headers = {'Authorization': 'Basic ' + btoa(token + ':""')};
 
-// Get our API routes
-
 const app = express();
 
-// Parsers for POST data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// Point static path to dist
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Catch all other routes and return the index file
 app.use(serveStatic(__dirname, {'index': ['public/index.html']}))
-
 
 app.get('/api/saved_search', (req, res) => {
     request.get({url: API + 'saved_search', headers: headers}, (err, response, body) => {
@@ -54,18 +45,8 @@ app.get('/api/activity', (req, res) => {
     });
 });
 
-/**
- * Get port from environment and store in Express.
- */
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || '3001';
 app.set('port', port);
 
-/**
- * Create HTTP server.
- */
 const server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
 server.listen(port, () => console.log(`API running on localhost:${port}`));
--- public/index.js
+++ public/index.js
@@ -41,31 +41,50 @@ app.directive("eventDropChart", function($window) {
             var d3 = $window.d3;
             var colors = d3.scaleOrdinal(d3.schemeCategory10);
             var eventColors = ['#aa3333', '#3333aa'];
+            var formatTime = d3.timeFormat("%e %B, %Y");
                        
             var data = [];
             for (var i = 0 ; i < names.length ; i ++) {
                 var innerData = [];
                 events[i].map(d => {
                     if (d._type === 'Email' || d._type === 'Call') {
-                        innerData.push({date: new Date(d.date_created), type: d._type === 'Email' ? 0 : 1});                        
+                        innerData.push({date: new Date(d.date_created), type: d._type === 'Email' ? 0 : 1, name: d.user_name, note: d._type === 'Call' ? d.note : d.body_text, duration: d._type === 'Call' ? d.duration : 0});                        
                     }
                 });
                 data.push({name: names[i].name, data: innerData});
             }
 
+            var div = d3.select("body").append("div")
+                .attr("class", "tooltip")
+                .style("opacity", 0);
+          
             var eventDropsChart = d3.chart.eventDrops()
                 .date(d => d.date)
                 .start(new Date('2016-08-08T15:05:15+00:00'))
                 .eventLineColor((d, index) => colors(index))
                 .eventColor(d => eventColors[d.type])
                 .mouseover(d => {
-
+                    div.transition()
+                        .duration(200)
+                        .style("opacity", .9);
+                    div.html("<p>" + d.note + "</p>" + (d.duration > 0 ? ("<p class='text-right'>" +  formatDuration(d.duration) + "</p>") : "") + "<p class='text-right'>" + formatTime(d.date) + "</p><h4 class='text-right'>" + d.name + "</h4>")
+                        .style("left", (d3.event.pageX - 30) + "px")
+                        .style("top", (d3.event.pageY + 10) + "px");
+                })
+                .mouseout(d => {
+                    div.transition()
+                        .duration(500)
+                        .style("opacity", 0);
                 });
 
             d3.select("#drop_event").datum(data)
                 .call(eventDropsChart);
 
             d3.select("svg").attr("width", window.innerWidth - 200);
+
+            function formatDuration(seconds) {
+                return parseInt(seconds / 60) > 0 ? parseInt(seconds / 60) + " mins" : '' + parseInt(seconds % 60) > 0 ? parseInt(seconds % 60) + " secs" : '';
+            }
         }
     };
 });
